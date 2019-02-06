43679151b19297d4e0fc7242fdadfb1de575e09e
xaviguasch <xaviguasch@gmail.com>
2019-02-06 15:09:16 +0100

    interacting with bitcoinaverage.com api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 
 const app = express()
 
@@ -11,6 +12,11 @@ app.get('/', function (req, res) {
     res.sendFile(`${__dirname}/index.html`)
 })
 
+app.post('/', function (req, res) {
+    request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', function (error, response, body) {
+        console.log(body)
+    })
+})
 
 
 app.listen(3000, function () {
